01e4546773905e0c08b2de90bd0223eb834510b9
Shadvonic <moxeymarcus28@gmail.com>
2025-03-07 09:26:19 -0500

    Add project files.

---
+++ ConsoleUI/ShoppingCartModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class ShoppingCartModel
+    {
+        public List<ProductModel> cart { get; set; }
+    }
+}
